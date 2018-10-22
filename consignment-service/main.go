9eb0461c0929209e5503e2ba15b66d0f2dc86175
Victor Kuznetsov <vic@spotify.com>
2018-10-22 15:26:57 +0200

    Add vessel service

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -5,6 +5,7 @@ import (
 
 	micro "github.com/micro/go-micro"
 	pb "github.com/whitesyn/shippy/consignment-service/proto/consignment"
+	vesselProto "github.com/whitesyn/shippy/vessel-service/proto/vessel"
 	"golang.org/x/net/context"
 )
 
@@ -12,27 +13,27 @@ const (
 	port = ":50051"
 )
 
-// IRepository Interface for repository to store consignments
-type IRepository interface {
+// Repository Interface for repository to store consignments
+type Repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
 	GetAll() []*pb.Consignment
 }
 
-// Repository - Dummy repository, this simulates the use of a datastore
+// ConsignmentsRepository - Dummy repository, this simulates the use of a datastore
 // of some kind. We'll replace this with a real implementation later on.
-type Repository struct {
+type ConsignmentsRepository struct {
 	consignments []*pb.Consignment
 }
 
 // Create consignment
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+func (repo *ConsignmentsRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
 	return consignment, nil
 }
 
 // GetAll consignments from repository
-func (repo *Repository) GetAll() []*pb.Consignment {
+func (repo *ConsignmentsRepository) GetAll() []*pb.Consignment {
 	return repo.consignments
 }
 
@@ -41,14 +42,28 @@ func (repo *Repository) GetAll() []*pb.Consignment {
 // in the generated code itself for the exact method signatures etc
 // to give you a better idea.
 type service struct {
-	repo IRepository
+	repo         Repository
+	vesselClient vesselProto.VesselServiceClient
 }
 
 // CreateConsignment - we created just one method on our service,
 // which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.CreateResponse) error {
-	// save our consignment
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity:  int32(len(req.Containers)),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+
+	// We set the VesselId as the vessel we got back from our vessel service
+	req.VesselId = vesselResponse.Vessel.Id
+
 	consignment, err := s.repo.Create(req)
 	if err != nil {
 		return err
@@ -67,8 +82,6 @@ func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *
 }
 
 func main() {
-	repo := &Repository{}
-
 	// Create a new service
 	srv := micro.NewService(
 		// This name must match the package name given in protobuf definition
@@ -76,11 +89,14 @@ func main() {
 		micro.Version("latest"),
 	)
 
+	repo := &ConsignmentsRepository{}
+	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
+
 	// Init will parse the command line flags
 	srv.Init()
 
 	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 
 	if err := srv.Run(); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
