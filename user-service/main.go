65fba5862d4d783a2bc8d45db900d73d15b43c83
Victor Kuznetsov <vic@spotify.com>
2018-10-24 11:51:10 +0200

    Fix user-cli

--- user-service/main.go
+++ user-service/main.go
@@ -9,10 +9,15 @@ import (
 
 func main() {
 	db, err := CreateConnection()
-	if err != nil {
-		log.Panicf("Could not connect to database - %v", err)
-	}
 	defer db.Close()
+	if err != nil {
+		log.Panicf("Could not connect to database: %v", err)
+	}
+	// Automatically migrates the user struct
+	// into database columns/types etc. This will
+	// check for changes and migrate them each time
+	// this service is restarted.
+	db.AutoMigrate(&pb.User{})
 
 	repo := &UsersRepository{db}
 
